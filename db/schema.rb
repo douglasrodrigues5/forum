4e26964948a3c9e9ca3cf258ed2a662daa98aef5
Douglas Rodrigues <douglasrodriguespg@gmail.com>
2017-03-29 06:20:58 -0700

    Changed db to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170327190418) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "pessoas", force: :cascade do |t|
     t.string   "name"
     t.integer  "age"
