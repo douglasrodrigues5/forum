47200d0c872dc9c8f4049156a4e8674864687be7
douglasrodrigues5 <douglasrodriguesguima@gmail.com>
2017-04-22 09:55:46 -0300

    closes #9

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+
   resources :people
   resources :posts do
     resources :comments
   end
 
 
-  get '/' =>'people#index', :as => "root"
+  get '/' =>'welcome#index', :as => "root"
+  get '/forum' => 'people#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
