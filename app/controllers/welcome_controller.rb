7b5c1ca25e0773f728ff22b776bc281f92253598
Douglas Rodrigues <douglasrodriguesguima@gmail.com>
2017-05-02 11:54:47 -0300

    User done.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,17 @@
 class WelcomeController < ApplicationController
+  helper_method :resource_name, :resource, :devise_mapping
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
   def index
     @posts = Post.all
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170501203101) do
+ActiveRecord::Schema.define(version: 20170502140552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20170501203101) do
     t.integer  "person_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
     t.index ["person_id"], name: "index_posts_on_person_id", using: :btree
   end
 
